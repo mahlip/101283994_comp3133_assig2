5edd0209ad911b03859e2453e0be95e0fc8bf4ad
moxDroid Labs Inc <pritam.world@gmail.com>
2022-03-29 19:12:46 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'week12_angular_apollo_graphql';
+}
